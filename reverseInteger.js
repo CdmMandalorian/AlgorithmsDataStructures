0e04b1f71762e6b770530fbce15b015ad95f4f8e
CdmMandalorian <68617257+CdmMandalorian@users.noreply.github.com>
2022-01-27 12:53:37 -0800

    reverseInteger Algorithm

--- reverseInteger.js
+++ reverseInteger.js
@@ -21,5 +21,8 @@
 //  Solution:   //
 
     var reverse = function(x) {
-        
+        let limit = 2147483648;
+        let k = x < 0 ? -1 : 1;
+        let n = Number(String(Math.abs(x)).split('').reverse().join(''));
+        return n > limit ? 0 : n * k;
     };
